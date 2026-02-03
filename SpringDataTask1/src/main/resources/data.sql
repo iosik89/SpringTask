a88b8115455ae620d12d82d9d7c56496f0f96d67
iosik89 <golemios89@gmail.com>
2026-02-03 15:19:38 +0300

    BookLibrary

---
+++ SpringDataTask1/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO BOOKS (id, title, author, publication_year)
+VALUES (random_uuid(), 'Book1', 'Author1', 2008);
+
+INSERT INTO BOOKS (id, title, author, publication_year)
+VALUES (random_uuid(), 'Book2', 'Author2', 2016);
+
+INSERT INTO BOOKS (id, title, author, publication_year)
+VALUES (random_uuid(), 'Book3', 'Author3', 2022);
